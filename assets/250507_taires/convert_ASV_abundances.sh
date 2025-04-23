affdc8ec462069be997f52e1c032e92a99fec8a2
palec87 <david@stanka.de>
2025-04-23 12:19:25 +0100

    added files tania, texts improvements

---
+++ assets/250507_taires/convert_ASV_abundances.sh
@@ -0,0 +1,10 @@
+awk -F'\t' '
+NR==2 {
+  printf "#NAME"
+  for (i=2; i<NF; i++) printf "\t%s", $i
+  print ""
+}
+NR>2 && !/^#/ {
+  for (i=1; i<NF; i++) printf "%s\t", $i
+  print ""
+}' ASV_table_original.txt > ASV_table.tsv
---
+++ assets/250507_taires/convert_ASV_taxonomy.sh
@@ -0,0 +1,29 @@
+# Remove trailing characters (spaces or tabs) and convert Windows-style line endings to Unix-style
+sed -e 's/[ \t]*$//' -e 's/\r$//' ASV_table_original.txt > ASV_table_cleaned.txt
+
+awk -F'\t' '
+BEGIN {
+  OFS = "\t"
+  print "#TAXONOMY", "Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species"
+}
+NR > 1 && !/^#/ {
+  # Reset taxonomy output array
+  for (i = 1; i <= 7; i++) tax[i] = ""
+
+  # Split taxonomy string
+  split($NF, rawtax, "; ")
+
+  # Clean taxonomy ranks and store them
+  for (i = 1; i <= length(rawtax); i++) {
+    gsub(/^.*__/, "", rawtax[i])
+    tax[i] = rawtax[i]
+  }
+
+  # Print OTU ID and cleaned taxonomy ranks
+  print $1, tax[1], tax[2], tax[3], tax[4], tax[5], tax[6], tax[7]
+}
+' ASV_table_cleaned.txt > taxonomy_dp_tabs.tsv
+
+sed '/^[ \t]*$/d' taxonomy_dp_tabs.tsv > taxonomy.tsv
+rm taxonomy_dp_tabs.tsv
+rm ASV_table_cleaned.txt
